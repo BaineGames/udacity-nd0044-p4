57d0d97e1e4f13e77e85f6cdcf15a6f8c141830b
BaineGames <noah.baine@gmail.com>
2020-04-13 14:27:07 -0700

    build check permissions function following lesson 4 vid 4 on rbac

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -75,6 +75,17 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            "code":"invalid_permissions",
+            "description":"Permissions not included in JWT"
+        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            "code":"unauthorized",
+            "description":"Permission Denied! Y u do dis?!"
+        }, 403)
 
     raise Exception('Not Implemented')
 
